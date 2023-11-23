4dd3179e798ed0aa9992c22563358f09a9247f43
Walter Park <neodust223@gmail.com>
2023-11-23 10:18:34 +0900

    challange day4
    로그용 미들웨어 morgan 설치

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
 const handleHome = (req, res) => {
   return res.send("<h1>You succeed</h1>");
@@ -11,6 +13,8 @@ const handleHome = (req, res) => {
 const handleLogin = (req, res) => {
   return res.send("Login here");
 };
+
+app.use(logger);
 
 app.get("/", handleHome);
 app.get("/login", handleLogin);
