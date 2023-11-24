0fc96b37dd846baf7080697a5537a5df7172f325
Walter Park <neodust223@gmail.com>
2023-11-24 15:46:02 +0900

    challange day5
    URL에 parameter를 전달할 때 정규식을 사용해서 형식을 지정해줄 수 있다.

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,9 +3,9 @@ import { see, edit, upload, deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
-videoRouter.get("/:id", see);
-videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/delete", deleteVideo);
+videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+videoRouter.get("/upload", upload); //URL파라메터 입력 받는 부분에 정규표현식으로 숫자라고 적었으므로 이게 아래로 와도 상관없음.
 
 export default videoRouter;
