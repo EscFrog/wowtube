ef4c73e09c96e96d1363559c8edd46cfaaa81e03
Walter Park <neodust223@gmail.com>
2023-11-24 11:32:59 +0900

    challange day5
    라우터 개념 이해를 위한 스파게티 코드

--- src/server.js
+++ src/server.js
@@ -5,19 +5,23 @@ const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
-
-const handleHome = (req, res) => {
-  return res.send("<h1>You succeed</h1>");
-};
-
-const handleLogin = (req, res) => {
-  return res.send("Login here");
-};
-
 app.use(logger);
 
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+const globalRouter = express.Router();
+const handleHome = (req, res) => res.send("<h1>Home</h1>");
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+const handleEditUser = (req, res) => res.send("<h1>Edit User</h1>");
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+const handleWatchVideo = (req, res) => res.send("<h1>Watch Video</h1>");
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`Server is listening on port http://localhost:${PORT} 🚀`);
